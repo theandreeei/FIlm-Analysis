c084d2766932980a8a5c2be3439c0edc7fc45d93
theandreeei <vertru112211@gmail.com>
2024-11-09 18:24:35 +0200

    Added README.md, requirements.txt, main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
 
 
-df = pd.read_excel('Dataset.xlsx')
+# Open the dataset
+url = 'film_analysis/movies_metadata.csv'
+movies_df = pd.read_csv(url)
+
+# Output infos about the dataset
+movies_df.info()
+print(movies_df.head())
+print(movies_df.describe())
+
 
 
 
