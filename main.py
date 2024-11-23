bce9253ed41b15eda0b117c48f9fb5f99709c90a
theandreeei <vertru112211@gmail.com>
2024-11-23 18:23:40 +0200

    Edited README.md and modified main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 import ast
 
 
@@ -39,11 +39,48 @@ movies_df.dropna(subset=['budget', 'revenue'], inplace=True)
 #---------
 
 # print(movies_df['release_date'])
-movies_df['release_date'] = pd.to_datetime(movies_df['release_date'], errors='coerce').dt.year
-# print(movies_df['release_date'])
+movies_df['release_year'] = pd.to_datetime(movies_df['release_date'], errors='coerce').dt.year
+# print(movies_df['release_year'])
 
 #---------
 
 movies_df['runtime'] = pd.to_numeric(movies_df['runtime'], errors='coerce')
-print(movies_df['runtime'])
+# print(movies_df['runtime'])
+
+#---------
+
+genres_exploded = movies_df[['title', 'release_year', 'budget', 'revenue', 'genres']].explode('genres')
+print(genres_exploded)
+
+genre_count = genres_exploded['genres'].value_counts()
+print(genre_count)
+
+# Visualization of genres
+plt.figure(figsize=(10, 6))
+sns.barplot(x=genre_count.index, y=genre_count.values)
+plt.title('Number of movies by genre')
+plt.xlabel('Genre')
+plt.ylabel('Number of movies')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+plt.show()
+
+#---------
+
+movies_df['popularity'] = pd.to_numeric(movies_df['popularity'], errors='coerce')
+popularity_trend = movies_df.groupby('release_year')['popularity'].mean()
+
+# Visualization trends of popularity
+
+plt.figure(figsize=(12, 6))
+popularity_trend.plot()
+plt.title('Average movies popularity by years')
+plt.xlabel('Year')
+plt.ylabel('Popularity')
+# plt.xticks(rotation=45)
+plt.tight_layout()
+
+plt.show()
+
 
